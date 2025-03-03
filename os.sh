3b90b635accf7f3cc2bda3660a3094c80a2d69a1
DiDian <99331551+DiDianYew@users.noreply.github.com>
2025-03-03 17:20:15 +0800

    v0.0.2
    Complete all echo print

--- os.sh
+++ os.sh
@@ -47,9 +47,9 @@ while true; do
                     read selection
                     selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
 
-                    if ["$selection" = "q"]; then
+                    if [ "$selection" = "q" ]; then
                         break 2 
-                    elif ["$selection" = "y"]; then
+                    elif [ "$selection" = "y" ]; then
                         break 
                     else
                         echo "Invalid choice. Please enter choice again."
@@ -59,15 +59,174 @@ while true; do
             ;;
         "S")
             while true; do
-            	clear
-                echo "Functionality not implemented yet."
-                echo -n "Press Enter q to return to the menu..."
-                read selection
-                selection=$(echo "$selection") 
-		if [ "$selection" = "q" ]; then
-		    break
-		fi
+                clear
+                echo "Search a Patron Details"
+                echo ""
+                echo -n "Enter Patron ID: "
+                read patronid
+                echo "First Name: "
+                echo "Last Name: "
+                echo "Mobile Number: "
+                echo "Birth Date (MM-DD-YYYY): "
+                echo "Membership type: "
+                echo "Joined Date (MM-DD-YYYY): "
+                echo ""
+                echo "Press (q) to return to Patron Maintenance Menu."
+                echo ""
+                while true; do
+                    echo -n "Search another patron? (y)es or (q)uit: "
+                    read selection
+                    selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
+
+                    if [ "$selection" = "q" ]; then
+                        break 2 
+                    elif [ "$selection" = "y" ]; then
+                        break 
+                    else
+                        echo "Invalid choice. Please enter choice again."
+                    fi
+                done
             done
+            ;;
+        "U")
+            clear
+                echo "Update a Patron Details"
+                echo ""
+                echo -n "Enter Patron ID: "
+                read patronid
+                echo "First Name: "
+                echo "Last Name: "
+                echo -n "Mobile Number: "
+                read mobilenumber
+                echo -n "Birth Date (MM-DD-YYYY): "
+                read birthdate
+                echo "Membership type: "
+                echo "Joined Date (MM-DD-YYYY): "
+                echo ""
+                echo "Press (q) to return to Patron Maintenance Menu."
+                echo ""
+                echo -n "Are you sure you want to \e[1mUPDATE\e[0m the above Patron Details? (y)es or (q)uit: "
+                read selection
+                selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
+
+                if [ "$selection" = "q" ]; then
+                    continue 
+                elif [ "$selection" = "y" ]; then
+                     echo "Save info to file (Need change)"  # save info 
+                else
+                    echo "Invalid choice. Please enter choice again."
+                fi
+            ;;
+        "D")
+            clear
+                echo "Delete a Patron Details"
+                echo ""
+                echo -n "Enter Patron ID: "
+                read patronid
+                echo "First Name: "
+                echo "Last Name: "
+                echo "Mobile Number: "
+                echo "Birth Date (MM-DD-YYYY): "
+                echo "Membership type: "
+                echo "Joined Date (MM-DD-YYYY): "
+                echo ""
+                echo "Press (q) to return to Patron Maintenance Menu."
+                echo ""
+                echo -n "Are you sure you want to \e[1mDELETE\e[0m the above Patron Details? (y)es or (q)uit: "
+                read selection
+                selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
+
+                if [ "$selection" = "q" ]; then
+                    continue 
+                elif [ "$selection" = "y" ]; then
+                     echo "Delete info to file (Need change)"  # save info 
+                else
+                    echo "Invalid choice. Please enter choice again."
+                fi
+            ;;
+        "L")
+            clear
+                col_width=20
+                echo "Patron Details Sorted by Last Name"
+                echo ""
+                printf "%-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s\n" "Last Name" "First Name" "Mobile Number" "Joined Date" "Membership Type"
+                printf "%-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s\n" "test" "test" "test" "test" "test"
+                echo ""
+                echo "Press (q) to return to Patron Maintenance Menu."
+                echo ""
+                echo -n "Would you like to export the report as ASCII text file? (y)es (q)uit: "
+                read selection
+                selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
+
+                if [ "$selection" = "q" ]; then
+                    continue 
+                elif [ "$selection" = "y" ]; then
+                     echo -n "Expor into (file_name). Please enter to continue..."
+                     read selection 
+                     if [ "$selection" = "" ]; then
+                        continue 
+                     else
+                        echo "Invalid choice. Please press enter again."
+                     fi 
+                else
+                    echo "Invalid choice. Please enter choice again."
+                fi
+            ;;
+        "P")
+            clear
+                col_width=20
+                echo "Patron Details Sorted by Patron ID"
+                echo ""
+                printf "%-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s\n" "Patron ID" "Last Name" "First Name" "Mobile Number" "Birth Date"
+                printf "%-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s\n" "test" "test" "test" "test" "test"
+                echo ""
+                echo "Press (q) to return to Patron Maintenance Menu."
+                echo ""
+                echo -n "Would you like to export the report as ASCII text file? (y)es (q)uit: "
+                read selection
+                selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
+
+                if [ "$selection" = "q" ]; then
+                    continue 
+                elif [ "$selection" = "y" ]; then
+                     echo -n "Expor into (file_name). Please enter to continue..."
+                     read selection 
+                     if [ "$selection" = "" ]; then
+                        continue  
+                     else
+                        echo "Invalid choice. Please press enter again."
+                     fi 
+                else
+                    echo "Invalid choice. Please enter choice again."
+                fi
+            ;;
+        "J")
+            clear
+                col_width=20
+                echo "Patron Details Sorted by Joined Date"
+                echo ""
+                printf "%-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s\n" "Joined Date" "Patron ID" "Last Name" "First Name" "Mobile Number"
+                printf "%-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s %-${col_width}s\n" "test" "test" "test" "test" "test"
+                echo ""
+                echo "Press (q) to return to Patron Maintenance Menu."
+                echo ""
+                echo -n "Would you like to export the report as ASCII text file? (y)es (q)uit: "
+                read selection
+                selection=$(echo "$selection" | tr 'A-Z' 'a-z') 
+
+                if [ "$selection" = "q" ]; then
+                    continue 
+                elif [ "$selection" = "y" ]; then
+                     echo -n "Expor into (file_name). Please enter to continue..."
+                     read selection 
+                     if [ "$selection" = "" ]; then
+                        continue 
+                     else
+                        echo "Invalid choice. Please press enter again."
+                     fi 
+                else
+                    echo "Invalid choice. Please enter choice again."
+                fi
             ;;
         "Q")
             exit
